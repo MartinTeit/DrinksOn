1c75fa825674d4e172dc07a61bd28d499e8f3cd6
MartinTeit <Martinteit@icloud.com>
2019-06-04 13:38:40 +0200

    BOOM Bruger kan oprettes lokalt

--- app/src/main/java/com/example/drinkson/registerUser.java
+++ app/src/main/java/com/example/drinkson/registerUser.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class registerUser extends AppCompatActivity {
 
@@ -13,13 +14,20 @@ public class registerUser extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_create_user);
+        final EditText userNameBox = (EditText) findViewById(R.id.createUsername);
+
+        final EditText fullNameBox = (EditText) findViewById(R.id.createPassword);
 
         userCreated = (Button) findViewById(R.id.userCreated);
         userCreated.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                user newUser = new user();
+                newUser.id = userNameBox.getText().toString();
+                newUser.name = fullNameBox.getText().toString();
+                //System.out.println(fullNameBox.getText().toString());
                 openLaunch();
             }
         });
