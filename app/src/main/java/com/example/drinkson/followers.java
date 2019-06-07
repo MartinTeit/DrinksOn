ed640cdc9d8fb5f471cb8c38986ba839f3ccccb3
MartinTeit <Martinteit@icloud.com>
2019-06-07 14:02:59 +0200

    Magnus ting

--- app/src/main/java/com/example/drinkson/followers.java
+++ app/src/main/java/com/example/drinkson/followers.java
@@ -1,8 +1,12 @@
 package com.example.drinkson;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class followers extends AppCompatActivity {
 
@@ -12,5 +16,14 @@ public class followers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_followers);
+
+        List<String> followers = new ArrayList<>();
+        followers.add("Fisse");
+
+        String display = "Hej";
+        private TextView text = (TextView) findViewById(R.id.my_text_view);
+        text.setText(display);
     }
+
+
 }
