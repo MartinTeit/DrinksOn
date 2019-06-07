2986f64a749ea123759132100f88d506e7232e03
MartinTeit <Martinteit@icloud.com>
2019-06-07 12:55:23 +0200

    Link indsat til side filer

---
+++ app/src/main/java/com/example/drinkson/rack.java
@@ -0,0 +1,13 @@
+package com.example.drinkson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class rack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rack);
+    }
+}
