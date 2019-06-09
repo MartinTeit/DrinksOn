79f75dc978145c6952027265bdbbe615bc14902f
MartinTeit <Martinteit@icloud.com>
2019-06-09 12:22:08 +0200

    ScrollView

--- app/src/main/java/com/example/drinkson/location.java
+++ app/src/main/java/com/example/drinkson/location.java
@@ -1,13 +1,37 @@
 package com.example.drinkson;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class location extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class location extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng odense = new LatLng(55.39594, 10.38831);
+        map.addMarker(new MarkerOptions().position(odense).title("Odense"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(odense));
     }
 }
