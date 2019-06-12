16e1d706404839b2e2481dab339fe8ff835ddd98
MartinTeit <Martinteit@icloud.com>
2019-06-12 17:09:56 +0200

    MAGNUS NU STJÆLER DU FANDENEDMER IKKE MINE KNAPPER IGEN!

--- app/src/main/java/com/example/drinkson/MapsActivity.java
+++ app/src/main/java/com/example/drinkson/MapsActivity.java
@@ -171,6 +171,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
 
         drinkMap = googleMap;
+        //onLocationChanged(userLastLocation);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
