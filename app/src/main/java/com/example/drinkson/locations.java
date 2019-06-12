369ab82cf9521067db34f12678ff59f7180d26ca
vongroes <mgjkollund@gmail.com>
2019-06-12 13:31:36 +0200

    added locationsharing

--- app/src/main/java/com/example/drinkson/locations.java
+++ app/src/main/java/com/example/drinkson/locations.java
@@ -1,14 +1,18 @@
 package com.example.drinkson;
 
 import android.Manifest;
+import android.arch.persistence.room.Room;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -18,63 +22,63 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import org.w3c.dom.Text;
+
 public class locations extends AppCompatActivity {
 
+    Button button;
+    TextView textView;
     private TextView latitude;
     private TextView longitude;
-    private TextView altitude;
-    private TextView accuracy;
-    private TextView speed;
-    private TextView sensorType;
-    private TextView updatesOnOff;
-    private ToggleButton switchGpsBalanced;
-    private ToggleButton locationOnOff;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private static final int MY_PERMISSION_REQUEST_FINE_LOCATION = 101;
+    private Repository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locations);
+        button = (Button) findViewById(R.id.share);
+        textView = (TextView) findViewById(R.id.tvLatitude);
+
+        final localdatabase database = Room.databaseBuilder(getApplicationContext(),
+                localdatabase.class, "Danskere").build();
+        final DAO dao = database.getDAO();
+        final EditText target = findViewById(R.id.sharewith);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                final messages location = new messages();
+
+                long id;
+
+                location.sender = currentuser.getCurrentUser();
+
+                location.body = textView.getText().toString();
+                location.receiver = target.toString();
+                location.stamp = System.currentTimeMillis();
+
+                id = repository.insertMessage(location);
+                location.id = (int) id;
+                repository.remotePost(Repository.MESSAGES, JSONConverter.encodeMessages(location));
+
+
+                new AsyncTask<Void, Void, Void>() {
+                    protected Void doInBackground(Void... voids) {
+                        dao.insertMessage(location);
+                        return null;
+                    }
+                }.execute();
+
+            }
+        });
 
         //initialuse View
         latitude = (TextView) findViewById(R.id.tvLatitude);
         longitude = (TextView) findViewById(R.id.tvLongitude);
-        altitude = (TextView) findViewById(R.id.tvAltitude);
-        accuracy = (TextView) findViewById(R.id.tvAccuracy);
-        speed = (TextView) findViewById(R.id.tvSpeed);
-        sensorType = (TextView) findViewById(R.id.tvSensor);
-        updatesOnOff = (TextView) findViewById(R.id.tvUpdates);
-        switchGpsBalanced = (ToggleButton) findViewById(R.id.tbGps_Balanced);
-        locationOnOff = (ToggleButton) findViewById(R.id.tvLocationOnOff);
 
-        switchGpsBalanced.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (switchGpsBalanced.isChecked()) {
-                    //using GPS only
-                    sensorType.setText("GPS");
-                } else {
-                    //using balanced power accuracy
-                    sensorType.setText("Cell Tower and WiFi");
-                }
-
-            }
-        });
-
-        locationOnOff.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (locationOnOff.isChecked()) {
-                    //location updates on
-                    updatesOnOff.setText("On");
-                } else {
-                    //location updates off
-                    updatesOnOff.setText("Off");
-                }
-
-            }
-        });
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
@@ -84,17 +88,6 @@ public class locations extends AppCompatActivity {
                     if (location != null) {
                         latitude.setText(String.valueOf(location.getLatitude()));
                         longitude.setText(String.valueOf(location.getLongitude()));
-                        accuracy.setText(String.valueOf(location.getAccuracy()));
-                        if (location.hasAltitude()) {
-                            altitude.setText(String.valueOf(location.getAltitude()));
-                        } else {
-                            altitude.setText("No altitude available");
-                        }
-                        if (location.hasSpeed()) {
-                            speed.setText(String.valueOf(location.getSpeed()) + "m/s");
-                        } else {
-                            speed.setText("No speed available");
-                        }
 
                     }
                 }
