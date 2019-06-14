b7535b3ff589d689adef31bda935c4a655e7fd36
LaustKadziola <LaustKadziola@gmail.com>
2019-06-14 18:17:17 +0200

    Created escape condition and comments in chat

--- app/src/main/java/com/example/drinkson/chat.java
+++ app/src/main/java/com/example/drinkson/chat.java
@@ -21,7 +21,6 @@ public class chat extends AppCompatActivity {
     private RecyclerView.LayoutManager layoutManager;
     private Button sendButton;
     private EditText text;
-    private List<messages> myMessages;
 
     private Repository repository;
 
@@ -37,12 +36,14 @@ public class chat extends AppCompatActivity {
         repository = new Repository(this);
         List<messages> someMessages;
 
+        // gets message from the remote database
         if(repository.findUser(receiver).name.startsWith("%GRP")){
             someMessages = repository.remoteGetMessages(receiver);
         } else {
             someMessages = repository.remoteGetMessages(currentuser.getCurrentUser());
         }
 
+        // Input the messages into the local database to update for new messages
         for(messages m : someMessages){
             repository.insertMessage(m);
         }
@@ -52,6 +53,7 @@ public class chat extends AppCompatActivity {
         text = findViewById(R.id.editText);
         sendButton = findViewById(R.id.send);
 
+        // Listener for when sendButton is pressed
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,13 +68,15 @@ public class chat extends AppCompatActivity {
 
     }
 
-
+    // Updates the RecyclerView containing the messages
     private  void updateMessages(){
         List<messages> myMessages;
 
         List<messages> messagesInThisConversation = new ArrayList<>();
         messages = new ArrayList<>();
 
+        // If this is a group chat get all the group messages
+        // else get all the messages between current user an the other user
         if(repository.findUser(receiver).name.startsWith("%GRP")){
             myMessages = repository.getGroupChat(receiver);
             for(messages m: myMessages){
@@ -89,7 +93,7 @@ public class chat extends AppCompatActivity {
             }
         }
 
-        recyclerView = (RecyclerView) findViewById(R.id.messages);
+        recyclerView = findViewById(R.id.messages);
 
         layoutManager = new LinearLayoutManager(this);
         ((LinearLayoutManager) layoutManager).setStackFromEnd(true);
@@ -102,28 +106,47 @@ public class chat extends AppCompatActivity {
 
     private void sendMessage(String text){
 
+        // don't send the message if it is empty
         if(!text.equals("")){
             long id;
             int responseCode;
+            int maxIteration = 10;
+            int iteration = 0;
+            messages newMessage;
 
+            // random number generator to gat a random id for the message
             Random randInt = new Random();
 
-            // create new message
-            messages newMessage = new messages();
+            // creates a new message
+            newMessage = new messages();
             newMessage.sender = currentuser.getCurrentUser();
             newMessage.receiver = receiver;
             newMessage.body = text;
             newMessage.stamp = System.currentTimeMillis();
 
             do {
+                // Generate a random id for the message
                 id = randInt.nextInt();
                 newMessage.id = (int) id;
                 responseCode = repository.remotePost(Repository.MESSAGES, JSONConverter.encodeMessages(newMessage));
-            } while (responseCode == HttpsURLConnection.HTTP_CONFLICT);
+                iteration++;
 
-            repository.insertMessage(newMessage);
+                // If there was a conflict on the remote database try again with another id
+                // while max iteration isn't exceeded
+            } while (responseCode == HttpsURLConnection.HTTP_CONFLICT && iteration<maxIteration);
 
-            updateMessages();
+            if(iteration >= maxIteration && responseCode == HttpsURLConnection.HTTP_CONFLICT) {
+                // If max iteration exceeded write it in console. and don't post the message
+                System.out.println("max iteration exceeded");
+            } else {
+                // If nothing went wrong insert the message in the local database
+                repository.insertMessage(newMessage);
+
+                // Update the view
+                updateMessages();
+            }
+
+
         }
 
     }
